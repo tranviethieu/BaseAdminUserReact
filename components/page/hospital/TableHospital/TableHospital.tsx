9596e0aaa43184d6f94df8ef48e08497040b72bc
DESKTOP-3NBEK92\TranVietHieu <hieutran26498@gmail.com>
2023-11-09 21:11:57 +0700

    add

--- components/page/hospital/TableHospital/TableHospital.tsx
+++ components/page/hospital/TableHospital/TableHospital.tsx
@@ -1,103 +1,91 @@
-import { useRouter } from "next/router";
-import { Fragment, useEffect, useState } from "react";
+import {useRouter} from 'next/router';
+import {Fragment, useEffect, useState} from 'react';
 import Pagination from '~/components/controls/Pagination';
-import { useSelector } from "react-redux";
-import useDebounce from "~/common/hooks/useDebounce";
-import { RootState } from "~/redux/store";
-import { httpRequest } from "~/services";
+import {useSelector} from 'react-redux';
+import useDebounce from '~/common/hooks/useDebounce';
+import {RootState} from '~/redux/store';
+import {httpRequest} from '~/services';
 import {useQuery} from 'react-query';
-import accounts from "~/services/acccounts";
-import {TypeData} from  '../MainHospital/interfaces'
-import Tippy from "@tippyjs/react";
-import Table from "~/components/common/Table";
-import DataWrapper from "~/components/common/DataWrapper";
-import styles from "./TableHospital.module.scss";
+import accounts from '~/services/acccounts';
+import {TypeData} from '../MainHospital/interfaces';
+import Tippy from '@tippyjs/react';
+import Table from '~/components/common/Table';
+import DataWrapper from '~/components/common/DataWrapper';
+import styles from './TableHospital.module.scss';
 import Noti from '~/components/common/DataWrapper/components/Noti';
 import clsx from 'clsx';
 import backgrounds from '~/constants/images/backgrounds';
-import Popup from "~/components/common/Popup";
-import PopupDetailExam from "~/components/popups/PopupDetailExam";
-
+import Popup from '~/components/common/Popup';
+import PopupDetailExam from '~/components/popups/PopupDetailExam';
+import {BsEye} from 'react-icons/bs';
+import CheckBox from '~/components/controls/CheckBox';
+import Switch from '~/components/controls/Switch';
 
 function TableHospital() {
-    const router = useRouter();
+	const router = useRouter();
 	const {uuid, ...rest} = router.query;
-    const {token} = useSelector((state: RootState) => state.auth);
-    const {
-		hospitalStatus,
-		hospitalType,
-		hospitalKeyword,
-	} = useSelector((state: RootState) => state.sort).sort;
+	const {token} = useSelector((state: RootState) => state.auth);
+	const {hospitalStatus, hospitalType, hospitalKeyword} = useSelector(
+		(state: RootState) => state.sort
+	).sort;
 	const keyword = useDebounce(hospitalKeyword || '', 1000);
-    ///------
-    const [limit, setLimit] = useState<number>(10);
-    const [page, setPage] = useState<number>(1);
+	///------
+	const [limit, setLimit] = useState<number>(10);
+	const [page, setPage] = useState<number>(1);
 
-    //====
-    const [dataTable, setDataTable] = useState<any[]>([]);
-    const [total, setTotal] = useState(0)
-    
-    const {data,error, isLoading} = useQuery(
-        [
+	const [dataTable, setDataTable] = useState<any[]>([]);
+	const [total, setTotal] = useState(0);
+	const {data, isLoading} = useQuery({
+		queryKey: [
+			'key_hospital',
 			keyword,
 			hospitalStatus?.id,
 			hospitalType?.id,
 			limit,
 			page,
 			token,
-        ],
-        ()=>{
-            httpRequest({
-                http: accounts.getListHospitals({
+		],
+		queryFn: async () => {
+			const res = await httpRequest({
+				http: accounts.getListHospitals({
 					token: token!,
 					Keyword: keyword,
 					Status: hospitalStatus?.id || null,
 					CustomerType: hospitalType?.id || null,
 					Limit: limit,
 					Page: page,
-				}).then((data)=>{
-                    debugger
-                    //setDataTable(data?.data?.items || [])
-                    //setTotal(data?.data?.pagination?.totalCount || 0)
-                    return {
-                        data: data?.data?.items || [],
-                        total: data?.data?.pagination?.totalCount || 0,
-                        isLoading: false
-                    }
-                })
-            })
-        }
-    );
-
-      // eslint-disable-next-line react-hooks/rules-of-hooks
-    //   useEffect(() => {
-    //     setDataTable(data?.data || []);
-    //   }, [data]);
-
-    console.log(dataTable)
-    return (
+				}),
+			});
+			if (res) {
+				return {
+					items: res.items || [],
+					total: res.pagination.totalCount || 0,
+				};
+			}
+		},
+	});
+	return (
 		<Fragment>
 			<div className={styles.container}>
 				<DataWrapper
 					loading={isLoading}
-					// noti={
-                    //     <Noti
-					// 		img={backgrounds.table_noti}
-					// 		title='Chưa có lịch'
-					// 		des='Hiện tại bạn chưa có lịch hẹn khám nào.'
-					// 	/>
-						
-					// }
+					data={data?.items}
+					noti={
+						<Noti
+							img={backgrounds.table_noti}
+							title='Chưa có lịch'
+							des='Hiện tại bạn chưa có lịch hẹn khám nào.'
+						/>
+					}
 				>
 					<div className={styles.table}>
 						<Table
-							data={dataTable}
+							data={data?.items}
 							column={[
 								{
 									className: styles.title,
 									title: 'Mã đặt khám',
 									render: (item: TypeData) => (
-                                        
 										<Tippy
 											content='Xem chi tiết'
 											placement='top'
@@ -130,25 +118,76 @@ function TableHospital() {
 										</Tippy>
 									),
 								},
-                                {
-									title: 'Số điện thoại',
+								{
+									title: 'LOẠI KHÁCH HÀNG',
 									render: (item: TypeData) => (
 										<>{item.customerType.name}</>
 									),
-								}
-								
+								},
+								{
+									title: 'TRẠNG THÁI',
+									render: (item: TypeData) => (
+										<>
+											<div></div>{' '}
+											{item.status == 1
+												? 'Hoạt động'
+												: 'Dừng hoạt động'}
+										</>
+									),
+								},
+								{
+									title: '',
+									render: (item: TypeData) => (
+										<div className={styles.control}>
+											{/* Xem chi tiết */}
+											<Tippy
+												content='Xem chi tiết'
+												placement='top'
+											>
+												<div
+													className={clsx(
+														styles.item,
+														styles.detailb
+													)}
+													onClick={() => {
+														router.replace(
+															{
+																pathname:
+																	router.pathname,
+																query: {
+																	...router.query,
+																	uuid: item.uuid,
+																},
+															},
+															undefined,
+															{
+																shallow: true,
+																scroll: false,
+															}
+														);
+													}}
+												>
+													<BsEye
+														size={16}
+														color='#303C4F'
+													/>
+												</div>
+											</Tippy>
+										</div>
+									),
+								},
 							]}
 						/>
 					</div>
 				</DataWrapper>
-				{dataTable.length > 0 && (
+				{data?.items.length > 0 && (
 					<div className={styles.pagination}>
 						<Pagination
 							pageCurrent={page}
 							pageSize={limit}
 							onSetPage={setPage}
 							onSetPageSize={setLimit}
-							totalItem={total}
+							totalItem={data?.total}
 							textResult='lịch hẹn khám'
 						/>
 					</div>
